ea1cb3c750456ab07337cf1f50bd1243e686a33b
Ryan Wei <rainmanwy@gmail.com>
2025-12-02 17:16:18 +0800

    ouput for power test

--- 06_sql/rollout.sh
+++ 06_sql/rollout.sh
@@ -48,9 +48,7 @@ for i in $(find "${PWD}" -maxdepth 1 -type f -name "*.${BENCH_ROLE}.*.sql" -prin
     
     start_log
     if [ "${EXPLAIN_ANALYZE}" == "false" ]; then
-      if [ "${LOG_DEBUG}" == "true" ]; then
-        log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE=\"\" -f ${PWD}/${i} | wc -l"
-      fi
+      log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE=\"\" -f ${PWD}/${i} | wc -l"
       tuples=$(
         psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE="" -f "${PWD}/${i}" | wc -l
         exit ${PIPESTATUS[0]}
@@ -61,9 +59,7 @@ for i in $(find "${PWD}" -maxdepth 1 -type f -name "*.${BENCH_ROLE}.*.sql" -prin
     else
       myfilename=$(basename "${i}")
       mylogfile=${TPC_DS_DIR}/log/${myfilename}.single.explain_analyze.log
-      if [ "${LOG_DEBUG}" == "true" ]; then
-        log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE=\"EXPLAIN ANALYZE\" -f ${PWD}/${i} > ${mylogfile}"
-      fi
+      log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE=\"EXPLAIN ANALYZE\" -f ${PWD}/${i} > ${mylogfile}"
       psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE="EXPLAIN ANALYZE" -f "${PWD}/${i}" > "${mylogfile}"
       if [ $? != 0 ]; then
         tuples="-1"
